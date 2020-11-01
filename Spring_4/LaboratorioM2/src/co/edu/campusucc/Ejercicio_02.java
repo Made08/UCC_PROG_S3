5cd8a6f431b2d96495ae8bad598b45fe003e72f2
madelem chico <madelenchicovelasco@gmail.com>
2020-10-31 22:00:44 -0500

    Taller del Laboratorio M2

---
+++ Spring_4/LaboratorioM2/src/co/edu/campusucc/Ejercicio_02.java
@@ -0,0 +1,29 @@
+package co.edu.campusucc;
+
+public class Ejercicio_02 {
+	public static void main(String[] args) {
+		System.out.println("  __♥_                                      __♥__");
+		System.out.println(" ( ___ )-------´*•.¸(*•.¸♥¸.•*´)¸.•*´----- ( ___ )");
+		System.out.println("  | ♥ |  ╔════════════════════════════════╗ | ♥ |");
+		System.out.println("  | ♥ |  ║  AUTOR:  MADELEM CHICO VELASCO ║ | ♥ |");
+		System.out.println("  | ♥ |  ║   FECHA:    31-OCTUBRE-2020    ║ | ♥ |");
+		System.out.println("  | ♥ |  ║      PROGRAMA DE ING. SISTEMAS ║ | ♥ |");
+		System.out.println("  | ♥ |  ║             ID: 759383         ║ | ♥ |");
+		System.out.println("  |___|  ╚════════════════════════════════╝ |___|");
+		System.out.println(" (_____)-------.¸.•*(¸.•*´♥`*•.¸)`*•. -----(_____)");
+		
+		int a,b;
+		boolean s=false,s1=false;
+		
+		a=10;
+		b=18;
+		s=a<b;
+		s1=a<=b;
+		
+		System.out.print("El valor final de s es:" +s);
+		System.out.print("El valor final es s1 es:" +s1);
+		
+		
+		
+	}
+}
